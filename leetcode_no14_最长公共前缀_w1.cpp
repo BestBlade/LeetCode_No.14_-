a5b3eb059def6368197d6146f1251fc9cd5d1b96
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-08-21 20:25:47 +0800

    Add files via upload

---
+++ leetcode_no14_最长公共前缀_w1.cpp
@@ -0,0 +1,103 @@
+﻿/*------------------------------------------------------------------|
+编写一个函数来查找字符串数组中的最长公共前缀。						|
+																	|
+如果不存在公共前缀，返回空字符串 ""。								|
+																	|
+示例 1:																|
+																	|
+输入: ["flower","flow","flight"]									|
+输出: "fl"															|
+示例 2:																|
+																	|
+输入: ["dog","racecar","car"]										|
+输出: ""															|
+解释: 输入不存在公共前缀。											|
+说明:																|
+																	|
+所有输入只包含小写字母 a-z 。										|
+																	|
+来源：力扣（LeetCode）												|
+链接：https://leetcode-cn.com/problems/longest-common-prefix		|
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	横向比较法
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了91.73%的用户
+*	内存消耗：7.1 MB, 在所有 C++ 提交中击败了20.09%的用户
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string match(const string &str1,const string &str2)
+{
+	int length = min(str1.size(), str2.size());
+	 
+	int  result = {0};
+
+	while (result < length && str1[result] == str2[result])
+	{
+		result++;
+	}
+	return str1.substr(0, result);
+}
+
+string longestCommonPrefix(vector<string>& strs) 
+{
+	int nums = strs.size();
+
+	string result;
+
+	if (nums == 0)
+	{
+		return result;
+	}
+
+	if (nums == 1)
+	{
+		return strs[0];
+	}
+
+	if (nums >= 2)
+	{
+		result = strs[0];
+		for (int i = 1; i < nums; i++)
+		{
+			result = match(result, strs[i]);
+			if (!result.size())
+			{
+				break;
+			}
+		}
+	}
+	
+	//if (nums >= 2)														/*	同样的思路，这种写法就比上面的慢一倍	*/
+	//{
+	//	string common = match(strs[0], strs[1]);
+
+	//	for (int i = 2; i < nums; i++)
+	//	{
+	//		if (common == "")
+	//		{
+	//			return common;
+	//		}
+	//		common = match(common, strs[i]);
+	//	}
+	//	result = common;
+	//}
+
+	return result;
+}
+
+int main()
+{
+	vector<string> strs = { "flower","flow","flight" };
+
+	//
+
+	//
+
+	cout << longestCommonPrefix(strs) << endl;
+}
---
+++ leetcode_no14_最长公共前缀_w2.cpp
@@ -0,0 +1,73 @@
+﻿/*------------------------------------------------------------------|
+编写一个函数来查找字符串数组中的最长公共前缀。						|
+																	|
+如果不存在公共前缀，返回空字符串 ""。								|
+																	|
+示例 1:																|
+																	|
+输入: ["flower","flow","flight"]									|
+输出: "fl"															|
+示例 2:																|
+																	|
+输入: ["dog","racecar","car"]										|
+输出: ""															|
+解释: 输入不存在公共前缀。											|
+说明:																|
+																	|
+所有输入只包含小写字母 a-z 。										|
+																	|
+来源：力扣（LeetCode）												|
+链接：https://leetcode-cn.com/problems/longest-common-prefix		|
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	纵向比较法
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了91.73%的用户
+*	内存消耗：6.9 MB, 在所有 C++ 提交中击败了54.79%的用户
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs)
+{
+	int nums = strs.size();
+	string result;
+
+	if (nums == 0)
+	{
+		return result;
+	}
+
+	if (nums == 1)
+	{
+		return strs[0];
+	}
+
+	if (nums >= 2)
+	{
+		int length = strs[0].size();
+		for (int i = 0; i < length; i++)
+		{
+			char c = strs[0][i];
+			for (int j = 0;j < nums; j++)
+			{
+				if (i == strs[j].size()|| strs[j][i] != c)											/*	匹配不上的情况肯定是比匹配的上的情况少的，因此判断匹配不上的情况更好一些	*/
+				{
+					return strs[0].substr(0, i);
+				}
+			}
+		}
+	}
+	return strs[0];
+}
+
+
+int main()
+{
+	vector<string> strs = { "flower","flow","flight" };
+
+	cout << longestCommonPrefix(strs) << endl;
+}
